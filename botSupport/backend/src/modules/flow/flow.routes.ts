ccb59ca7aca586c30045b815767df69d07e3b439
ruandiegl <ruandiegl33@gmail.com>
2026-08-10 17:52:29 -0300

    Organiza raiz do projeto e protege arquivos sensíveis

--- botSupport/backend/src/modules/flow/flow.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { flowController } from "./flow.controller.js";
-
-const router = Router();
-
-router.get("/flow", (req, res) => flowController.get(req, res));
-router.put("/flow", (req, res) => flowController.update(req, res));
-
-export default router;
--- botSupport/frontend/src/pages/my-conversations/index.tsx
+++
@@ -1,5 +0,0 @@
-import QueuePage from "@/pages/queue";
-
-export default function MyConversationsPage() {
-  return <QueuePage onlyMine />;
-}
